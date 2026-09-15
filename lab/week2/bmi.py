b9e2e5e4ef28e7030edda0d63d760aa0a8135cef
yunseong <gimyunseong439@gmail.com>
2026-09-15 12:52:43 +0900

    09월15일_주석추가

--- lab/week2/bmi.py
+++ lab/week2/bmi.py
@@ -2,13 +2,16 @@
 def cal_bmi(height, weight) -> float :
     """
     result : bmi 계산한 값.
-    state : 아래에 있는 bmi에 따른 상태
+    (float 자료형의 값을 출력할 것임.)
+    state : 아래에 있는 bmi에 따른 상태 
+    (state : "저체중", "표준", "과체중", "비만")
     """
     result = weight / ((height)*0.01)**2
     state = ""
 
     """
     bmi의 result에 따라, state를 설정.
+    [출력형태] (state, result) 즉, 튜플로 출력.
     """
 
     if 20 > result : state = "저체중"
@@ -22,9 +25,11 @@ def main() :
     height = float(input("키를 입력해주세요. (단위 : cm) : "))
     weight = float(input("몸무게를 입력해주세요. (단위 : kg) : "))
 
+    #튜플 언패킹
     state ,result = cal_bmi(height, weight)
     print(f"당신의 bmi는 {result:.3f}입니다.")
     print(f"당신은 {state} 입니다.")
 
 if __name__ == "__main__" :
     main()
+    
